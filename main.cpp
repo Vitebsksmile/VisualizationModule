1aae85688c51fc21844b6e8a9063bd3c3d708d5b
Vitebsksmile <vitebsksmile@mail.ru>
2026-01-29 13:50:52 +0300

    init ImageViewer

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQuickWindow>
+#include <QApplication>
+
+#include "mainwindow.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-
+    MainWindow widget;
+    widget.setWindowTitle("OIS");
+    widget.show();
 
 
     return a.exec();
